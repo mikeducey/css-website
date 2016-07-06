2903344ddf2f21c4f55a613575bc8687e77968c9
mikeducey <michaeljducey@gmail.com>
2016-07-06 14:52:31 -0500

    Add files via upload

---
+++ moviesiteindex.php
@@ -0,0 +1,191 @@
+<?php $thisPage="movie-css"; ?>
+<?php $title="Example of CSS Work"; ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="moviecss.css">
+		<title><?php echo $title; ?></title>
+	</head>
+	<body>
+	<div class = "leftNavBar">
+		<div class = "search">
+				<p><div class = "search__text">Search movies...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="mglass.png" alt= "magnifying glass" height = "12" width = "12"></div></p>
+		</div>
+		<div class = "leftNavBar__navigation">
+			<h4><div class = "li-navigation_categories navigation__categories">Main</div></h4>
+			<div class = "navigation__listText">
+				<ul>
+					<li><div class = "navigation__listImg"><img src="playingicon.png" alt= "playing icon" height = "15" width = "15"></div> Playing </li>
+					<li><div class = "navigation__listImg"><img src="discovericon.png" alt= "discover icon" height = "15" width = "15"></div> Discover </li>
+					<li><div class = "navigation__listImg"><img src="activityicon.png" alt= "activity icon" height = "15" width = "15"></div> Activity </li>
+					<li><div class = "navigation__listImg"><img src="topchartsicon.png" alt= "top charts icon" height = "15" width = "15"></div> Top Charts </li>
+					<li><div class = "navigation__listImg"><img src="newreleasesicon.png" alt= "new releases icon" height = "15" width = "15"></div> New Releases </li>
+				</ul>
+			</div>
+			<h4><div class = "li-navigation_categories navigation__categories">Your Movie Circles</div></h4>
+			<div class = "navigation__listText">
+				<ul>
+					<li><div class = "navigation__listImg"><img src="mustsee.png" alt= "must see" height = "15" width = "15"></div> This I Must See </li>
+					<li><div class = "navigation__listImg"><img src="thebest.png" alt= "the best" height = "15" width = "15"> The Best </li>
+				</ul>
+			</div>
+			<h4><div class = "li-navigation_categories navigation__categories">Collections</div></h4>
+			<div class = "navigation__listText">
+				<ul>
+					<li><div class = "navigation__listImg"><img src="meandbob.png" alt= "me and bob" height = "15" width = "15"> Me and Bob </li>
+					<li><div class = "navigation__listImg"><img src="travelmovies.png" alt= "travel movies" height = "15" width = "15"> Travel Movies </li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class = "mainsection">
+		<div class = "titleSection">
+			<div class = "title">
+				<p>Discover</p>
+			</div>
+			<div class = "titleTabs">
+				<p><div class = "titleTabs__activetab">Release Date |<div class = "titleTabs__nonactivetab">Popularity</div></div></p>
+			</div>
+			<div class = "titleSection__icons"><img src="refreshicon.png" alt= "refresh icon" height = "20" width = "20"><img src="entericon.png" alt= "enter icon" height = "21" width = "21"></div>
+		</div>
+		<div class = "moviesection">
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+			<div class = "movie">
+				<img src="examplemovie.png" alt= "example movie"><p><div class = "movie__titleText">The Great Gatsby</div><div class = "movie__infoText">Drama, Romance<br>143 min</div></p>
+			</div>
+		</div>
+	</div>
+	<div class = "rightNavBar">
+		<div class = "movieprofile">
+			<div class = "movieprofile__img">
+				<img src="profilepic.png" alt= "profile pic" height = "30" width = "30">
+			</div>
+			<div class = "movieprofile__name">Jack Dorsey</div>
+			<div class = "movieprofile__img">
+				<img src="profileicon.png" alt= "profile icon" height = "15" width = "15">
+			</div>
+		</div>
+		<div class = "friends">
+				<h4><div class = "friend_headers">Online</div></h4>
+					<ul>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="reddot.png" alt= "status icon" height = "8" width = "8"></div></li>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="reddot.png" alt= "status icon" height = "8" width = "8"></div></li>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="reddot.png" alt= "status icon" height = "8" width = "8"></div></li>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="reddot.png" alt= "status icon" height = "8" width = "8"></div></li>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="reddot.png" alt= "status icon" height = "8" width = "8"></div></li>
+					</ul>
+				<h4><div class = "friend_headers">Offline</div></h4>
+					<ul>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="greydot.png" alt= "status icon" height = "8" width = "8"></div></li>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="greydot.png" alt= "status icon" height = "8" width = "8"></div></li>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="greydot.png" alt= "status icon" height = "8" width = "8"></div></li>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="greydot.png" alt= "status icon" height = "8" width = "8"></div></li>
+						<li><div class = "friends__img"><img src="friendprofile.png" alt= "friend icon" height = "28" width = "28"></div><div class = "friends__text">Victor Ericson<br><div class = "friends__movieText">The Great Gatsby</div></div><div class = "friends__dot"><img src="greydot.png" alt= "status icon" height = "8" width = "8"></div></li>
+					</ul>
+		</div>
+		<div class = "chatwith">
+			<p><div class = "search__text">Chat with...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="mglass.png" alt= "magnifying glass" height = "12" width = "12"></div></p>
+		</div>
+	</div>
+
+	</body>
+</html>
